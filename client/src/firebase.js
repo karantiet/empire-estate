e3ed1cd55a35c7e7a3e7374d33092deaa743530e
Karan_Chhabra <kchhabra_be21@thapar.edu>
2024-04-28 14:26:27 +0530

    Initial Commit

---
+++ client/src/firebase.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = { apiKey: "AIzaSyDDSax2KyNcmTpTIQIn1Q8bY_Ecc_wnvq4", authDomain: "empire-estate-b4dcc.firebaseapp.com", projectId: "empire-estate-b4dcc", storageBucket: "empire-estate-b4dcc.appspot.com", messagingSenderId: "824080381742", appId: "1:824080381742:web:95fdfe406e4b283f29acbf" };
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
